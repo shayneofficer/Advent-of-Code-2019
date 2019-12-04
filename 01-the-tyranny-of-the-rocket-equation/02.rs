4774a4709966cff6ab35e53fa0c13edcfdd51be4
shayneofficer <shayneofficer@gmail.com>
2019-12-03 21:03:44 -0500

    reformatting

--- 01-the-tyranny-of-the-rocket-equation/02.rs
+++ 01-the-tyranny-of-the-rocket-equation/02.rs
@@ -8,12 +8,16 @@ fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
             if let Ok(ip) = line {
-                total += (ip.parse::<i32>().unwrap() / 3) - 2;
+                let mut mass = (ip.parse::<i32>().unwrap() / 3) - 2;
+                while mass > 0 {
+                    total += mass;
+                    mass = (mass / 3) - 2;
+                }
             }
         }
     }
 
-    println!("{}", total); // 3382284
+    println!("{}", total); // 5070541
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
